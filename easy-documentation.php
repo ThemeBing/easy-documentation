29cb66617b0991a3f85670165f2ecbaff4a11a66
ThemeBing <themebing@gmail.com>
2020-03-18 11:07:10 +0600

    Update easy-documentation.php

--- easy-documentation.php
+++ easy-documentation.php
@@ -74,7 +74,7 @@ class Easy_Documentation_Public {
 			'show_ui'               => true,
 			'show_in_menu'          => true,
 			'menu_position'         => 5,
-			'menu_icon'             => 'dashicons-cart',
+			'menu_icon'             => 'dashicons-format-aside',
 			'show_in_admin_bar'     => true,
 			'show_in_nav_menus'     => true,
 			'can_export'            => true,
