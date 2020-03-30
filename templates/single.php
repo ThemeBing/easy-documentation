e02783492e5d456a4d848dbcf6da4311e0352369
Farhan Khan <farhan01617@gmail.com>
2020-03-31 00:35:38 +0600

    up

--- templates/single.php
+++ templates/single.php
@@ -6,30 +6,32 @@
 
  get_header(); ?>
 
-	<div class="row">
-		<div class="col-lg-4">
-			<div class="docs-sidebar">
-				<ul>
-				<?php 
-				wp_list_pages( array(
-					'post_type' => 'easy_documentation',
-					'title_li'  => '',
-	        		'order'     => 'menu_order',
-			    ) ) ?>
-			    </ul>
-		    </div>
-		</div>
-		<div class="col-lg-8">
-			<div class="docs-wrapper">
-			<?php while ( have_posts() ) { the_post(); ?>
-				<?php the_title( '<h1 class="docs-title">','</h1>' ) ?>
-				<?php the_content() ?>
-				<?php if ( comments_open() || get_comments_number() ) { ?>					
-				<hr>
-				<?php
-					comments_template(); 
-				} ?>
-			<?php } ?>
+ 	<div class="container">
+		<div class="row">
+			<div class="col-lg-4">
+				<div class="docs-sidebar">
+					<ul>
+					<?php 
+					wp_list_pages( array(
+						'post_type' => 'easy_documentation',
+						'title_li'  => '',
+		        		'order'     => 'menu_order',
+				    ) ) ?>
+				    </ul>
+			    </div>
+			</div>
+			<div class="col-lg-8">
+				<div class="docs-wrapper">
+				<?php while ( have_posts() ) { the_post(); ?>
+					<?php the_title( '<h1 class="docs-title">','</h1>' ) ?>
+					<?php the_content() ?>
+					<?php if ( comments_open() || get_comments_number() ) { ?>					
+					<hr>
+					<?php
+						comments_template(); 
+					} ?>
+				<?php } ?>
+				</div>
 			</div>
 		</div>
 	</div>
